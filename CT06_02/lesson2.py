d4be8c8a091a01b7a564be59dedf5bd6e041721e
Elijah2107 <olgawong@hotmail.com>
2025-01-13 10:15:45 +0000

    Jan 13, 2025, 10:15 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -2,7 +2,7 @@
 #    say "hey!"
 #    move 10 steps
 
-# sa
+# set the value of  
 
 ######## Write the pseudocode in comments for task 2 here
 # Using comments, translate the code shown on screen into pseudocode.
