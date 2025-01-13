9ac5f6a2e35ab05a24a87b128ae0edff66cd3177
Elijah2107 <olgawong@hotmail.com>
2025-01-13 10:17:53 +0000

    Jan 13, 2025, 10:17 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -5,7 +5,8 @@
 # set the value of counter to 0
 # as long as the counter is equal to 50
 #      move by 10 steps
-#      turn right by 15
+#      turn right by 15 degrees
+
 
 ######## Write the pseudocode in comments for task 2 here
 # Using comments, translate the code shown on screen into pseudocode.
