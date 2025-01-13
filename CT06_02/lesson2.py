c3021846eb87f33df283b314f33e2cf41631a557
Elijah2107 <olgawong@hotmail.com>
2025-01-13 10:15:26 +0000

    Jan 13, 2025, 10:15 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -2,7 +2,7 @@
 #    say "hey!"
 #    move 10 steps
 
-# assign countr
+# assign counter 
 
 ######## Write the pseudocode in comments for task 2 here
 # Using comments, translate the code shown on screen into pseudocode.
