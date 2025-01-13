f67bd1bc9679ab039f8f4f8910b8543baa1eacb8
Elijah2107 <olgawong@hotmail.com>
2025-01-13 10:16:33 +0000

    Jan 13, 2025, 10:16 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -3,7 +3,8 @@
 #    move 10 steps
 
 # set the value of counter to 0
-# repeat until the counter is equal to 50#
+# repeat until the counter is equal to 50
+#       mo
 
 ######## Write the pseudocode in comments for task 2 here
 # Using comments, translate the code shown on screen into pseudocode.
