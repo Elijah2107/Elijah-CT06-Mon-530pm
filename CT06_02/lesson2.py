8b90a27ea9a980a777169711b01248a3ad9b8451
Elijah2107 <olgawong@hotmail.com>
2025-01-13 10:30:22 +0000

    Jan 13, 2025, 10:30 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -2,16 +2,16 @@
 #    say "hey!"
 #    move 10 steps
 
-
-
-######## Write the pseudocode in comments for task 2 here
-# Using comments, translate the code shown on screen into pseudocode.
-
 # assign 0 to "counter"
 # as long as the counter is equal to 50
 #      move by 10 steps
 #      turn right by 15 degrees
 #      change counter by 10
+
+
+######## Write the pseudocode in comments for task 2 here
+# Using comments, translate the code shown on screen into pseudocode.
+
 
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode
