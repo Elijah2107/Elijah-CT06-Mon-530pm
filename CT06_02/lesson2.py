066c1d33e17ccee9ecf06d1559efbbc6d8d3e04d
Elijah2107 <olgawong@hotmail.com>
2025-01-13 10:17:12 +0000

    Jan 13, 2025, 10:17 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -4,6 +4,7 @@
 
 # set the value of counter to 0
 # as long as the counter is equal to 50
+#      
 
 ######## Write the pseudocode in comments for task 2 here
 # Using comments, translate the code shown on screen into pseudocode.
