bd1d9c046024e5926f8f5b1a3cddb189d02d68be
Elijah2107 <olgawong@hotmail.com>
2025-01-13 09:55:07 +0000

    Jan 13, 2025, 9:55 AM

--- CT06_01/lesson1.py
+++ CT06_01/lesson1.py
@@ -1,4 +1,10 @@
-
+print("Hello world")
+print("My favourite food is chicken rice")
+print("My birthday is on 24 October 2013")
+print("My name is Elijah")
+print("I am 11 years old")
+print("I attend St. Joseph's Institution Junior")
+print("If I had $2000 as a reward, I would buy a PS5")
 print("Pour milk and cereal into a bowl.")
 print("Wake up and get out of bed.")
 print("Eat your breakfast.")
