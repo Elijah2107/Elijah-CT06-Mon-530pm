61631f818bf05bb912bb6f4b2b981f3dafb46d4a
Elijah2107 <olgawong@hotmail.com>
2025-01-13 10:16:43 +0000

    Jan 13, 2025, 10:16 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -4,7 +4,7 @@
 
 # set the value of counter to 0
 # repeat until the counter is equal to 50
-#       mo
+#       move
 
 ######## Write the pseudocode in comments for task 2 here
 # Using comments, translate the code shown on screen into pseudocode.
