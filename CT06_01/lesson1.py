472bf689c6f77bf91bcaeb47a32327d02830441e
Elijah2107 <olgawong@hotmail.com>
2025-01-13 09:56:11 +0000

    Jan 13, 2025, 9:56 AM

--- CT06_01/lesson1.py
+++ CT06_01/lesson1.py
@@ -6,7 +6,6 @@
 # print("I attend St. Joseph's Institution Junior")
 # print("If I had $2000 as a reward, I would buy a PS5")
 
-
-print("Pour milk and cereal into a bowl.")
 print("Wake up and get out of bed.")
+print("Pour milk and cereal into a bowl.")
 print("Eat your breakfast.")
