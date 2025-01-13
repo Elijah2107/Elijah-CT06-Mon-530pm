a37aa329bf401faa712bc11d168ddb89f1fd4d6a
Elijah2107 <olgawong@hotmail.com>
2025-01-13 10:14:49 +0000

    Jan 13, 2025, 10:14 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -1,6 +1,6 @@
 #Repeat 10 times:
 #    say "hey!"
-#
+#    move 10 steps
 
 ######## Write the pseudocode in comments for task 2 here
 # Using comments, translate the code shown on screen into pseudocode.
