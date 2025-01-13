42fbbb50d98f1daf24ef177fea3bf3fe24d648db
Elijah2107 <olgawong@hotmail.com>
2025-01-13 09:57:45 +0000

    Jan 13, 2025, 9:57 AM

--- CT06_01/lesson1.py
+++ CT06_01/lesson1.py
@@ -10,10 +10,10 @@ print("Wake up and get out of bed.")
 print("Pour milk and cereal into a bowl.")
 print("Eat your breakfast.")
 
-# print(Hello and how are you?)
+print("Hello and how are you?")
 
-# print("What is wrong with this"
+print("What is wrong with this")
 
-# print("Why is this not working?)
+print("Why is this not working?")
 
-# Prnt("Why is this not working?")
+Prnt("Why is this not working?")
