d73cef6cfabc70e61f61298bcd58ec8575f74b98
Elijah2107 <olgawong@hotmail.com>
2025-01-20 09:46:44 +0000

    Jan 20, 2025, 9:46 AM

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,8 +1,9 @@
 #print("Hello from lesson 3")
 
-# assign Adam with score 60
+# assign Adam with score 6
 # assign Brandon with score 58
 # assign Charlie with score 98
 # assign Daniel with score 79
 
 # take the sum of the students, and divide by 4
+# print out average score
