6aaed1ce4388d83f1416b4802c34ea438f7b2336
Elijah2107 <olgawong@hotmail.com>
2025-01-20 09:43:44 +0000

    Jan 20, 2025, 9:43 AM

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,6 +1,6 @@
 #print("Hello from lesson 3")
 
-# assign adam with score 60
-# assign brandon with score 58
+# assign Adam with score 60
+# assign Brandon with score 58
 # assign charlie with score 98
 # assign da
