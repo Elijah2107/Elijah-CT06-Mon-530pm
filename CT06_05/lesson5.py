ad7fc4871703324e3ad2bf6acbe59394827153f6
Elijah2107 <olgawong@hotmail.com>
2025-02-03 10:29:09 +0000

    Feb 3, 2025, 10:29 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -8,4 +8,5 @@
 #print("Happy " + str(age) + "th birthday " + name + "! " + message)
 
 for count in range(1000):
-    print("I will not sling mud a")
+    print("I will not sling mud at my friend ever!")
+
