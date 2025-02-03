457729fa7254834f624de0f4e9ae940810404270
Elijah2107 <olgawong@hotmail.com>
2025-02-03 10:14:59 +0000

    Feb 3, 2025, 10:14 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -2,7 +2,7 @@
 
 # recap 1
 
-name = input("What is the birthday person's name? ")
-age = input("What is the age of the person that he is turning this year? ")
-message = input("What is the personal message you are going to send? ")
-print("Happy " + age + "th birthday " + name + "! " + message)
+#name = input("What is the birthday person's name? ")
+#age = input("What is the age of the person that he is turning this year? ")
+#message = input("What is the personal message you are going to send? ")
+#print("Happy " + age + "th birthday " + name + "! " + message)
