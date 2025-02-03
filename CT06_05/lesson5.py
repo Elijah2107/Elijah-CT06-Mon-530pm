67b66d328dc6c610055e339389f52ca7862ebbbb
Elijah2107 <olgawong@hotmail.com>
2025-02-03 10:49:42 +0000

    Feb 3, 2025, 10:49 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -7,9 +7,14 @@
 #message = input("What is the personal message you are going to send? ")
 #print("Happy " + str(age) + "th birthday " + name + "! " + message)
 
-for count in range(1000):
-    print("I will not sling mud at my friend ever!")
+#or count in range(1000):
+    # print("I will not sling mud at my friend ever!")
 
-for abc in "I will not sling mud at my friend ever!":
-    print(abc)
+#for abc in "I will not sling mud at my friend ever!":
+#    print(abc)
 
+# task 1
+
+myname = input("What is your name? ")
+for letter in myname:
+    print("Give me a " + myname)
