030ee28b4e0bf24e97b1a569157c5e5d37940045
Elijah2107 <olgawong@hotmail.com>
2025-02-03 10:09:05 +0000

    Feb 3, 2025, 10:09 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -5,4 +5,4 @@
 name = input("What is the birthday person's name? ")
 age = input("What is the age of the person that he is turning this year? ")
 message = input("What is the personal message you are going to send? ")
-print("Happy " + )
+print("Happy " + age + "th")
