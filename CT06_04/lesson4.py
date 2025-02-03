dfd828036fb60bfa97b880526c2c8ee0028c4bea
Elijah2107 <olgawong@hotmail.com>
2025-02-03 09:39:09 +0000

    Feb 3, 2025, 9:39 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,1 +1,101 @@
 print("Hello from lesson 4")
+
+#red = 1
+#blue = 2
+#green = 3
+
+#red = red*3
+#blue = blue*5
+#green = green*4
+
+#total = red + blue + green 
+
+#print (total)
+
+#task 1a
+
+#myName = input("What is your name?")
+#print("Hello " + myName)
+
+#task 1b
+
+#favColour = input("What is your favourite colour?")
+#print ("My favourite colour is " + favColour)
+
+#task 1c
+
+#age = input("What is your age?")
+#print ("My age is " + age)
+
+#task 2a
+
+#name = input("What is your name? ")
+#print("Hi," + name)
+
+#task 2b
+
+#hobby = input("What is your hobby?")
+#print ("I enjoy " + hobby)
+
+#task 2c
+
+#place = input("Where would you like to visit?")
+#print ("I would love to visit " + place)
+
+#task 3a
+
+#qty = 1
+#amt = 17.95
+#print("Total Cost: $" , qty * amt)
+
+#num1 = 10
+#num2 = 5
+#ans = num1 + num2
+#print(str(num1) , " + " , str(num2) , " = " , str(ans))
+
+#age = input("What is your age?")
+#print("Your age next year is: " + str(int(age) + 1))
+
+#task 3b
+
+number = input("Give me a number")
+number = number * 2
+number = str(number)
+print("Double your number is " + number)
+
+#task 3c
+
+year_born = input("What year are you born in?")
+year_born = int(year_born)
+
+
+## Task 4: Variable Reassignment Basics
+
+#Assign the string "Hello" to a variable 'message'. 
+#Then reassign 'message' to the integer "10" and print it.
+
+#message = "Hello"
+#message = 10
+#print(message)
+
+#---------------------------------------------------------------
+
+## Task 5: Printing Full Name
+
+#Create 2 variables, 'firstName' and 'lastName', assigning your
+#first and last names to them. Then print them on the same line
+#with a space between them.
+
+firstName = Elijah
+lastName = Chua
+print(firstname + lastname)
+
+#---------------------------------------------------------------
+
+## Task 6: Age and Name Concatenation
+
+#Assign a name to the variable "user_name"
+#Assign an integer to the variable "user_age"
+#Use type conversion and string concatenation to print out
+#"[user_name] is [user_age] years old."
+ 
