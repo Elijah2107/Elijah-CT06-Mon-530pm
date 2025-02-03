ec76c48e63308e18a6f686e96665d04d2958cccc
Elijah2107 <olgawong@hotmail.com>
2025-02-03 10:07:51 +0000

    Feb 3, 2025, 10:07 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -4,4 +4,4 @@
 
 name = input("What is the birthday person's name? ")
 age = input("What is the age of the person that he is turning this year? ")
-message = input(" ")
+message = input("What is the per ")
