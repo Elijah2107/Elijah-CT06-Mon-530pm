db97b76b6f4a606e8fdd78a747507b42d4ae29e3
Elijah2107 <olgawong@hotmail.com>
2025-02-24 09:54:53 +0000

    Feb 24, 2025, 9:54 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -7,7 +7,7 @@ score_three = 75
 total = score_one + score_two + score_three
 
 average_score = total / 3
-average_score = str()
+average_score = str(average_score)
 student_name = "Alex"
 
 print("Average score for " + student_name + " is: " + average_score)
