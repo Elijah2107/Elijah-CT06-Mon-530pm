f5a18628f060bd91bd17078f09a2dabca8bc6b99
Elijah2107 <olgawong@hotmail.com>
2025-02-24 09:55:09 +0000

    Feb 24, 2025, 9:55 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,13 +1,13 @@
 # Recap 1
 
-score_one = 80
-score_two = 90
-score_three = 75
+# score_one = 80
+# score_two = 90
+# score_three = 75
 
-total = score_one + score_two + score_three
+# total = score_one + score_two + score_three
 
-average_score = total / 3
-average_score = str(average_score)
-student_name = "Alex"
+# average_score = total / 3
+# average_score = str(average_score)
+# student_name = "Alex"
 
-print("Average score for " + student_name + " is: " + average_score)
+# print("Average score for " + student_name + " is: " + average_score)
