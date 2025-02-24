bf4a80fef0c02a91596a36bc7c67ec025f68e6aa
Elijah2107 <olgawong@hotmail.com>
2025-02-24 09:53:01 +0000

    Feb 24, 2025, 9:53 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,1 +1,77 @@
-print("Hello from lesson 6")
+# numstud = int(input("How many students do you have? "))
+# sumscore = 0
+# for count in range(numstud):
+#     score = int(input("What is the score of the student? "))
+#     sumscore = sumscore + score
+# print(sumscore / numstud)
+
+#task 1a
+
+#for i in range(3):
+#    print("Hello, World!")
+
+#task 1b
+
+# for i in range(5):
+#     print(i)
+
+#task 1c
+
+#print("Hello, World!")
+
+#task 1d
+
+#if1 = 5
+
+#task 1e
+
+#print ("Hello, World!")
+
+#task 2a
+
+# age = 1332555453
+# print(age)
+
+#task 2b
+
+# name = "Alice"
+# print(name)
+
+#task 2c
+
+# x = 5
+# print(x)
+
+#task 2d
+
+#print("Hello, World!")
+
+#task 3a
+
+# age = 25
+# print(age + 1)
+
+#task 3b
+
+# number = 10
+# print(number - 5)
+
+#task 3c
+
+#print("Repeat" * 3)
+
+#task 3d
+
+# year = str(2023)
+# print("The year is " + year)
+
+#task 3e
+
+#x = 10
+#y = x / 2
+
+#task 3f
+
+# end = 5
+# for i in range(end):
+#     print(i)
