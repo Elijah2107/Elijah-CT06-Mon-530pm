b9dc2c86257b9f6f90159571e17bd7a1e56912a7
Elijah2107 <olgawong@hotmail.com>
2025-03-10 10:46:57 +0000

    Mar 10, 2025, 10:46 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -40,7 +40,7 @@
 import random
 num1 = random.randint(1,50)
 num2 = random.randint(1,50)
-question = "what is " + str(num1) + " + " + str(num2) + "?"
+question = "what is " + str(num1) + " + " + str(num2) + " ? "
 reply = input(question)
 reply = int(reply)
 hidden = num1 + num2
