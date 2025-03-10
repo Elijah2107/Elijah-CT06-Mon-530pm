d615d50e804f8e5c44f07ebb6da436c9d837be48
Elijah2107 <olgawong@hotmail.com>
2025-03-10 10:45:15 +0000

    Mar 10, 2025, 10:45 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -40,4 +40,4 @@
 import random
 num1 = random.randint(1,50)
 num2 = random.randint(1,50)
-question = "what is" + str(num1) + " + " + str(num2)
+question = "what is" + str(num1) + " + " + str(num2) + "?"
