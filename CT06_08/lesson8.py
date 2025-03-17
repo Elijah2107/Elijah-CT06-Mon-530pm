0a8c06943c9f4d88f6843d0847bac11b1eee2346
Elijah2107 <olgawong@hotmail.com>
2025-03-17 10:00:19 +0000

    Mar 17, 2025, 10:00 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -72,5 +72,5 @@ for count in range(questions):
     num1 = random.randint(1,10)
     num2 = random.randint(1,10)
     answer = input(str(num1) + " x " + str(num2) + " ")
-    print(answer == num1 * num2)
+    print(answer == (num1 * num2))
     
