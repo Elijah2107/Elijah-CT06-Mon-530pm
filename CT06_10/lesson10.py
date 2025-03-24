715212d969a37b52e8534e77d5e15d8bbabbd964
Elijah2107 <olgawong@hotmail.com>
2025-03-24 10:33:11 +0000

    Mar 24, 2025, 10:33 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,7 +1,9 @@
 # Task 1
 
-num = int(input("Give me a number "))
-if num >= 0:
-    print((str(num) + " " + "is postive." ))
-else:
-    print((str(num) + " " + "is negative."))
+# num = int(input("Give me a number "))
+# if num >= 0:
+#     print((str(num) + " " + "is postive." ))
+# else:
+#     print((str(num) + " " + "is negative."))
+
+# Task 2
