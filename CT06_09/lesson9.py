b6536675b83fccb309aa05dd23ad924333341a0f
Elijah2107 <olgawong@hotmail.com>
2025-03-24 09:38:16 +0000

    Mar 24, 2025, 9:38 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,8 +1,8 @@
 # Task 2
 
-days = 26
-if days > 25:
-    print("Remember to return your book! ")
+# days = 26
+# if days > 25:
+#     print("Remember to return your book! ")
 
 # Task 3
 
@@ -10,3 +10,4 @@ if days > 25:
 # randnum = random.randint(1,10)
 # guess = int(input("Give me a number between 1 to 10 "))
 # print(randnum == guess)
+
