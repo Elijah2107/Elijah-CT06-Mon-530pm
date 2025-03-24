fa0be5d1dbb5e21db3ec7963b3f171b3d44dedc5
Elijah2107 <olgawong@hotmail.com>
2025-03-24 10:45:22 +0000

    Mar 24, 2025, 10:45 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -27,9 +27,12 @@
 
 # Task 4
 
-num = int(input("Give me a number "))
-remainder = num % 2
-if remainder == 0:
-    print("The number is even")
-else:
-    print("The number is odd")
+# num = int(input("Give me a number "))
+# remainder = num % 2
+# if remainder == 0:
+#     print("The number is even")
+# else:
+#     print("The number is odd")
+
+# Task 5
+
