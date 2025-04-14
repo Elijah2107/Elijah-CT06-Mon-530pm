657b3ac874f2f4fcd31a513cd2c24ecb000e6e1b
Elijah2107 <olgawong@hotmail.com>
2025-04-14 10:23:54 +0000

    Apr 14, 2025, 10:23 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -4,3 +4,5 @@ word = input("Give me a 5 letter word that contains 1 'o' and 1 'e'. ")
 for letter in word:
     print(letter)
 
+if (letter in word == 5) and (letter in word == "e" and "o"):
+    print
