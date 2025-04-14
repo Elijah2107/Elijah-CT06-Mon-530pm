7795d4ae6ac084220f91d7b71ad1a2e1367c046c
Elijah2107 <olgawong@hotmail.com>
2025-04-14 10:25:13 +0000

    Apr 14, 2025, 10:25 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -1,10 +1,3 @@
 # Recap
 
 word = input("Give me a 5 letter word that contains 1 'o' and 1 'e'. ")
-for letter in word:
-    print(letter)
-
-if (letter in word == 5) and (letter in word == "e" and "o"):
-    print("Valid Word")
-else:
-    print("Invalid Word")
