5d9d25f7528667fb50402f55c3eaadf153a11374
Elijah2107 <olgawong@hotmail.com>
2025-04-21 11:00:23 +0000

    Apr 21, 2025, 11:00 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -5,14 +5,14 @@ print("Welcome to my bank!")
 answer = input("Choose the one of the following, withdraw, deposit, check balance, exit ")
 while not answer == exit:
     if answer == "withdraw":
-        withdrawal = input("How much money would you like to withdraw? ")
+        withdrawal = int(input("How much money would you like to withdraw? "))
         if balance < withdrawal:
             print("Cannot withdraw!")
         else:
             print(balance - withdrawal)
     
     if answer == "deposit":
-        deposit = input("How much would you like to deposit? ")
+        deposit = int(input("How much would you like to deposit? "))
         print(balance + deposit)
     
     if answer == "check balance":
