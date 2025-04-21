7697392fdb336b5a3d1473f898b7981a35e7ea2b
Elijah2107 <olgawong@hotmail.com>
2025-04-21 10:50:11 +0000

    Apr 21, 2025, 10:50 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -1,2 +1,7 @@
 # Recap
 
+balance = 1000
+answer = input("Choose the one of the following, withdraw, deposit, check balance, exit ")
+while not answer == exit:
+    if answer == withdraw:
+        withdrawal = 
