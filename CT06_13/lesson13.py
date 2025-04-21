028ebc862ca1c4c7930bf16b70a35640f0104fd0
Elijah2107 <olgawong@hotmail.com>
2025-04-21 10:51:11 +0000

    Apr 21, 2025, 10:51 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -6,3 +6,4 @@ answer = input("Choose the one of the following, withdraw, deposit, check balanc
 while not answer == exit:
     if answer == "withdraw":
         withdrawal = input("How much money would you like to withdraw? ")
+        
