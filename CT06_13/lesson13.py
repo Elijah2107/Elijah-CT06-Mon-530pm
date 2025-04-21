0b018641df1bea7b5d7ed5788796dbe38df0658e
Elijah2107 <olgawong@hotmail.com>
2025-04-21 10:50:26 +0000

    Apr 21, 2025, 10:50 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -4,4 +4,4 @@ balance = 1000
 answer = input("Choose the one of the following, withdraw, deposit, check balance, exit ")
 while not answer == exit:
     if answer == withdraw:
-        withdrawal = 
+        withdrawal = input("How much money would you like to withdraw? ")
