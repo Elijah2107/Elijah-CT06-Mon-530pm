623fe64143fe126dd965cb0cf77949017fe674dc
Elijah2107 <olgawong@hotmail.com>
2025-04-21 10:42:50 +0000

    Apr 21, 2025, 10:42 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -25,4 +25,47 @@
 
 # Task 3
 
-order = ""
+# order = ""
+# answer = input("What is your order? ")
+# while answer != "end":
+#     order = order + answer + ", "
+#     answer = input("What is your order? ")
+
+# print("You have ordered these items, ")
+# print(order)
+
+# Task 4
+
+# num = 10
+# print(num)
+# while num > 1:
+#     num = num -1
+#     print(num)
+# else:
+#     print("Happy New Year!")
+    
+# Task 5
+
+# import random
+# for count in range(5):
+#     num1 = random.randint(1,10)
+#     num2 = random.randint(1,10)
+#     question = "what is " + str(num1) + " + " + str(num2) + "? " 
+#     answer = input(question)
+#     answer = int(answer)
+#     hidden_answer = num1 + num2
+#     while not answer == hidden_answer:
+#         print("Wrong! Try Again")
+#         answer = input(question)
+#         answer = int(answer)
+#     else:
+#         print("You are Correct!")
+
+# Task 6
+
+import random
+num = 0
+num = random.randint(1,1000000000)
+while num != 4:
+    num = random.randint(1,1000000000)
+    print(num)
