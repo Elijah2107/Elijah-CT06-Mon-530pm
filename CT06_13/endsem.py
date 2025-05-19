bb828b77cd458853af80d9a992455db71e08464c
Elijah2107 <olgawong@hotmail.com>
2025-05-19 09:32:58 +0000

    May 19, 2025, 9:32 AM

---
+++ CT06_13/endsem.py
@@ -0,0 +1,42 @@
+# Task 1
+
+# num = 10
+# print(num)
+# while num < 200:
+#     num = num + 10
+#     print(num)
+
+# Task 2
+
+# password = "superpass123"
+# answer = input("What's the password? ")
+# if answer == password:
+#     print("Access Granted")
+# else:
+#     print("Access Denied")
+
+
+# Task 3a
+
+planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus"]
+print(planets[2])
+
+# Task 3b
+
+planets.append("neptune")
+print(planets)
+
+# Task 3c
+
+planets[3] = "muskworld"
+print(planets)
+
+# Task 3d
+
+del(planets[2])
+print(planets)
+
+# Task 3e
+
+for each_planet in planets:
+    print(each_planet)
--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -1,4 +1,34 @@
-# Task 1
+# Task 1a
 
-groceries = ["Apples", "Bananas", "Carrots", "Dates", "Eggs", "Flour", "Grapes", "Herbs", "Ice"]
-print(groceries)
+# groceries = ["Apples", "Bread", "Carrots", "Dates", "Eggs", "Flour", "Grapes", "Honey"]
+# print(groceries)
+
+# Task 1b
+
+# groceries[7] = "Herbs"
+# groceries.append("Ice")
+# print(groceries)
+
+# Task 1c
+
+# groceries.insert(1, "Banana")
+# print(groceries)
+
+# Task 1d
+
+# del(groceries[3])
+# print(groceries)
+
+# Task 2
+
+# groceries = ["Apples", "Bananas", "Carrots", "Dates", "Eggs", "Flour", "Grapes", "Herbs", "Ice"]
+# for item in groceries:
+#     print(item)
+#     if item == "Apples":
+#         print(item + ": I need 5 of these")
+    
+#     if item == "Carrots":
+#         print(item + ": I need 5 of these")
+    
+#     if item == "Grapes":
+#        print(item + ": Get the FarmFresh Brand")
