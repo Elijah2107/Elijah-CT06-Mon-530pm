b7fb4e0a1f6082cd74c209068446950dcbb82b9d
Elijah2107 <olgawong@hotmail.com>
2025-05-19 10:44:48 +0000

    May 19, 2025, 10:44 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -6,3 +6,21 @@
 # turtle.pendown()
 # turtle.forward(100)
 # turtle.mainloop()
+import turtle
+window = turtle.Screen()
+window.setup(width = 600, height = 400)
+import random
+turtle.goto(0,0)
+turtle.color("teal")
+turtle.speed(100)
+
+for count in range(10000):
+  turtle.up()
+  
+  randomX = random.randint(-150, 150)
+  randomY = random.randint(-100, 100)
+  turtle.goto(randomX, randomY)
+  turtle.write(str(randomX) + "," + str(randomY))
+  turtle.down()
+  
+window.mainloop()
