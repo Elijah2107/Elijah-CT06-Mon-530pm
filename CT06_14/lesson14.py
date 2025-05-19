3eb68b52750dc031c8cec642bcfdb17559cdcd79
Elijah2107 <olgawong@hotmail.com>
2025-05-19 10:23:06 +0000

    May 19, 2025, 10:23 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -1,8 +1,8 @@
 # Task 1
 
-import turtle
-window = turtle.Screen()
-window.setup(width = 600, height = 400)
-turtle.pendown()
-turtle.forward(100)
-turtle.mainloop()
+# import turtle
+# window = turtle.Screen()
+# window.setup(width = 600, height = 400)
+# turtle.pendown()
+# turtle.forward(100)
+# turtle.mainloop()
